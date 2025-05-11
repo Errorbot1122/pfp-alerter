439956cd77e83228b84ac801ba7273a63b6560d7
Errorbot1122 <errorbis@outlook.com>
2025-05-11 00:59:47 +0000

    feat: add mamber diff image renderer

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ A _discord bot_ that sends alert messages (with funny message) whenever a user c
 
  - [x] Sends message on profile change
  - [x] Members Opt-in and out
- - [ ] Funny messages **_(quips_**)
- - [ ] Image of changes sent when alerting
+ - [ ] Funny messages **_(quips)_**
+ - [x] Image of changes sent when alerting
  - [ ] Leaderboards and Rank
  - [ ] Adding your own quips
 
@@ -24,21 +24,20 @@ Here are a list of thing that we need help with!
 
  - [ ] More funny messages
  - [x] Logo
- - [ ] An member before and after image render
+ - [x] An member before and after image render
  - [ ] Leaderboard and Rank commands
  - [ ] Editing quips commands
 
 **Possible features**
  - [ ] Logo as SVG
+ - [ ] More diff arrows / user defined custom arrows
  - [ ] Better logging *
  - [ ] AI Generated quips **_(???)_**
  - [ ] Sharing Quips between guilds
 
 
 <details>
-    <summary>
-        <h5><small><em>*: More info about the logging<em></small></h5>
-    </summary>
+    <summary><h5><small><em>*: More info about the logging<em></small></h5></summary>
     <p>This is for sending logs/log ids to users when an error occurs. This system should be robust so it can work for other bots <small><em>(maybe a separate module, idk).</em></small></p>
     <p>It would be nice if we where able to send the full log to the user, so they can send it as an issue and get well versed contributes to help, however, logs most likely will have context that contain sensitive data and also most people using the bot, are most-likely... <em>dumb</em> so it wouldn't be of use to most people</p>
 </details>
--- bot.py
+++ bot.py
@@ -15,20 +15,21 @@ from collections.abc import Generator
 from datetime import datetime
 from random import choice
 from io import BytesIO
-from os import path
+import os
 
+import mimetypes
 import traceback
-import requests
 import inspect
 import json
 
-
 from dotenv import dotenv_values
 from discord.ext import commands, tasks
-from discord import Embed, app_commands
 from discord.app_commands import AppCommandError as AppCommandErrorBase
+from discord import Embed, app_commands, emoji
+from PIL import Image, ImageDraw, ImageFont, UnidentifiedImageError
+from cairosvg import svg2png
+import requests
 import discord
-
 
 JSON_SAVE_PERMS = {"sort_keys": True, "indent": 4, "separators": (", ", ": ")}
 
@@ -43,6 +44,14 @@ EMBED_DEFAULT_COLOR = discord.Color.purple()
 EMBED_ERROR_COLOR = discord.Color.magenta()
 
 FALLBACK_IMAGE = "./assets/not_found.png"
+FALLBACK_BYTES: bytes = bytes()
+with open(FALLBACK_IMAGE, "rb") as fallback:
+    FALLBACK_BYTES = fallback.read()
+
+ARROW_MAP = {
+    "default": "emoji.svg",
+    "emoji": "emoji.svg",
+}
 
 FUNNY_MESSAGES: dict[str, list[str]] = {
     "common": [
@@ -219,7 +228,8 @@ async def safe_call_async(
 
     if not inspect.iscoroutinefunction(func):
         return cast(
-            tuple[T, None] | tuple[None, BaseException],  # Cast to resolve type checker
+            # Cast to resolve type checker
+            tuple[T, None] | tuple[None, BaseException],
             safe_call(func, *args, **kwargs),
         )
 
@@ -262,7 +272,7 @@ def set_json_key(x: Any, key: Any, value: Optional[Any] = None):
         raise TypeError(f"Cannot create key on type '{type(x)}'! (Key: {str(key)})")
 
 
-def download_image(url: str) -> bytes:
+def download_image(url: str) -> BytesIO:
     """Download an image from a URL and return it as a BytesIO object.
 
     Parameters
@@ -284,7 +294,7 @@ def download_image(url: str) -> bytes:
     if response.status_code != 200:
         response.raise_for_status()
 
-    return response.content
+    return BytesIO(response.content)
 
 
 @overload
@@ -321,7 +331,7 @@ def get_save_from_guild(
     """
 
     file_path = "save/" + str(guild.id) + ".json"
-    if not path.exists(file_path):
+    if not os.path.exists(file_path):
         open(file_path, "w").close()
 
     file = open(file_path, "r+" if file_descriptor else "r")
@@ -452,7 +462,7 @@ def get_key(data: Any, key: Any | list[Any], allow_errors: bool = False) -> Any 
         current_key = key[key_index]
         if constrained_data is None or current_key not in constrained_data:
             if allow_errors:
-                raise KeyError(f"Cannot find key at {".".join(key[:key_index])}!")
+                raise KeyError(f"Cannot find key at {'.'.join(key[:key_index])}!")
             else:
                 return None
 
@@ -462,7 +472,7 @@ def get_key(data: Any, key: Any | list[Any], allow_errors: bool = False) -> Any 
             if allow_errors:
                 raise TypeError(
                     f"Cannot get key on type '{type(constrained_data)}' "
-                    + f"({".".join(key[:key_index])})"
+                    + f"({'.'.join(key[:key_index])})"
                 )
             else:
                 return None
@@ -470,34 +480,358 @@ def get_key(data: Any, key: Any | list[Any], allow_errors: bool = False) -> Any 
     return constrained_data
 
 
-@overload
+def get_overflow_text(
+    text: str,
+    width: int,
+    font: ImageFont.ImageFont,
+    suffix: str = "...",
+) -> str:
+    """Finds a substring of your `text` that dose not exceed `width` in pixels.
+    Adds `suffix` to the end if it dose exceed by default.
+
+    Parameters
+    ----------
+    text : str
+        the text to stop from overflowing
+    width : int
+        the maximum width for the text in pixels
+    font : ImageFont.ImageFont
+        the font used for gathering width data
+    suffix : str, optional
+        the t, by default "..."
+
+    Returns
+    -------
+    str
+        the overflow prevented text, with the `suffix` when it exceeds `width`
+    """
+
+    suffix_width = font.getlength(suffix)
+    if width < suffix_width:
+        return suffix  # handle extreme overflow
+
+    current_width = font.getlength(text)
+    if width > current_width:
+        return text
+
+    target_width = width - suffix_width
+    truncate_index = len(text)
+    while target_width < current_width and truncate_index >= 1:
+        current_width = font.getlength(text[:truncate_index])
+        truncate_index -= 1
+
+    return text[:truncate_index] + suffix
+
+
+def load_font(
+    path: str, is_truetype: Optional[bool] = None, **kwargs
+) -> ImageFont.ImageFont:
+    """Loads a `path` as an `ImageFont` safely, using the default if any errors
+    occur.
+
+    Parameters
+    ----------
+    path : str
+        the path to the font file
+    is_truetype : Optional[bool], optional
+        if the font is a truetype font or not. If `None` guesses using file mime.
+        By default None
+
+    Returns
+    -------
+    ImageFont.ImageFont
+        the font as an `ImageFont` or the default `ImageFont`
+    """
+    TRUE_TYPE_MIMES = ["font/ttf", "font/otf"]
+
+    if is_truetype == None:
+        font_mime = mimetypes.guess_type(path)[0]
+
+        if not font_mime.startswith("font/"):
+            print(f"'{path}' is not a font! (type: {font_mime})")
+            return ImageFont.load_default_imagefont()
+
+        if font_mime in TRUE_TYPE_MIMES:
+            is_truetype = True
+        else:
+            is_truetype = False
+
+    try:
+        if is_truetype:
+            return ImageFont.truetype(path, **kwargs)
+        else:
+            return ImageFont.load(path)
+    except OSError as e:
+        print(f"AN ERROR OCCURRED WHILE LOADING FONT! Loading default font. ({str(e)})")
+        return ImageFont.load_default()
+
+
+def open_as_image(uri: str, is_url: bool = False) -> Image.Image:
+    """Opens and image from either a **url** or **file path** and converts it
+    into a _Pillow_ `Image`
+
+    Parameters
+    ----------
+    uri : str
+        the url or path to an image
+    is_url : bool, optional
+        whether the `uri` argument is a **url** or a **path**, by default False
+
+    Returns
+    -------
+    PIL.Image.Image
+        the opened image as a _Pillow_ `Image`
+
+    Raises
+    ------
+    ValueError
+        when the opened image is determined to not be an image
+        (using mime types)
+    ValueError
+        when the image type is not supported
+    """
+    image_mime = mimetypes.guess_type(uri)[0]
+    if not image_mime.startswith("image/"):
+        raise ValueError(f"URI is not an image! (mime: {image_mime})")
+
+    image_bytes: bytes = bytes()
+    if is_url:
+        image_bytes = download_image(uri)
+    else:
+        with open(uri, "rb") as image_file:
+            image_bytes = image_file.read()
+
+    # Convert svg to png
+    image_mime = mimetypes.guess_type(uri)[0]
+    if image_mime == "image/svg+xml":
+        tmp_path = "./tmp/svg_tmp.png"
+        svg2png(bytestring=image_bytes, write_to=tmp_path)
+
+        image = Image.open(tmp_path)
+        os.remove(tmp_path)
+
+        return image
+    else:
+        try:
+            return Image.open(BytesIO(image_bytes))
+        except UnidentifiedImageError:
+            raise ValueError(
+                f"Images of mime {image_mime} are not supported by Pillow!"
+            )
+
+
+# TODO: Add mask for status icon too
+def mask_avatar(avatar: Image.Image) -> Image.Image:
+    """Helper function to add circle mask so an discord avatar
+
+    Parameters
+    ----------
+    avatar : PIL.Image.Image
+        the _Pillow_ `Image` of the avatar
+
+    Returns
+    -------
+    PIL.Image.Image
+        the new _Pillow_ `Image` of the avatar with the mask applied
+    """
+    # Circle mask
+    mask = Image.new(
+        "L", (avatar.size[0] * 3, avatar.size[1] * 3), 0
+    )  # Bigger to apply anti-aliasing for smoothing
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + mask.size, fill=255)
+
+    # Apply mask
+    new_avatar = avatar.copy()
+    new_avatar.putalpha(mask.resize(avatar.size))
+    return new_avatar
+
+
+def render_member(
+    member: discord.Member,
+    *,
+    width: int = 256,
+    height: int = 64,
+    avatar_padding: Optional[int] = None,
+    pfp: Optional[str] = None,
+    name: Optional[str] = None,
+) -> Image.Image:
+    """Recreates the member list item from **_Discord_** using _Pillow_.
+
+    Parameters
+    ----------
+    member : discord.Member
+        the base member to recreate
+        (`pfp` and `name` can override the corresponding values in here)
+    width : int, optional
+        how long in pixels the list item should be, by default 256
+    height : int, optional
+        how tall in pixels the list item should be, by default 64
+    avatar_padding : Optional[int], optional
+        the amount of padding that should be givin all around to the avatar in pixels,
+        by default `height // 8`
+    pfp : Optional[str], optional
+        an override for the url of the avatar for the member, by default None
+    name : Optional[str], optional
+        an override for the name of the member, by default None
+
+    Returns
+    -------
+    PIL.Image.Image
+        the generated `Image` of the member.
+    """
+    # TODO: Show status
+    # TODO: Show activities
+
+    BACKGROUND_COLOR = (41, 43, 47, 255)
+    NAME_FONT = load_font("assets/fonts/gg sans Bold.ttf", size=height / 3)
+
+    # Default color is black, instead of white.
+    name_color = (
+        member.color.to_rgb()
+        if member.color != discord.Color.default()
+        else (255, 255, 255)
+    ) + (255,)
+
+    pfp = pfp or member.display_avatar.url
+    name = name or member.display_name
+    avatar_padding = avatar_padding or (height // 8)
+
+    text_width = NAME_FONT.getlength(name)
+    ellipsis_width = NAME_FONT.getlength("...")
+
+    avatar_size = height - (avatar_padding * 2)
+    avatar = Image.open(
+        # TODO: Use .read() instead
+        safe_call(download_image, pfp)[0]
+        or BytesIO(FALLBACK_BYTES)
+    ).resize((avatar_size, avatar_size))
+
+    image = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+    draw = ImageDraw.Draw(image)
+    draw.rounded_rectangle(
+        (0, 0, width, height), height / 4, BACKGROUND_COLOR
+    )  # Background
+
+    masked_avatar = mask_avatar(avatar)
+    image.paste(
+        masked_avatar, (avatar_padding, avatar_padding), masked_avatar
+    )  # Avatar
+    draw.text(
+        ((avatar_padding * 2) + avatar_size, height / 2),
+        get_overflow_text(
+            name, width - (avatar_size + (avatar_padding * 2)), NAME_FONT
+        ),
+        font=NAME_FONT,
+        anchor="lm",
+        fill=name_color,
+    )  # Display Name
+
+    return image
+
+
+async def render_member_diff(
+    member: discord.Member,
+    before_name: Optional[str] = None,
+    before_pfp: Optional[str] = None,
+    arrow: str = "emoji",
+    *,
+    arrow_padding: Optional[int] = None,
+    height: int = 64,
+) -> Image.Image:
+    """Generates an image comparing 2 states of the same member
+    (before and after).
+
+    Parameters
+    ----------
+    member : discord.Member
+        the member to compare
+    before_name : Optional[str], optional
+        the old name of the member, by default None
+    before_pfp : Optional[str], optional
+        the old avatar url of the member, by default None
+    arrow : str, optional
+        the url or key arrow used in the comparison. If this is instead a value
+        in `ARROW_MAP` it will use that value instead. By default "emoji"
+    arrow_padding : Optional[int], optional
+        the (extra) padding around the arrow, by default `height // 6`
+    height : int, optional
+        the height (size) in pixels to generate the `Image`, by default 64
+
+    Returns
+    -------
+    PIL.Image.Image
+        the _Pillow_ `Image` that was generated
+    """
+    MEMBER_ITEM_ASPECT_RATIO = 4.5
+    border_padding = height // 8
+
+    before_name = before_name or member.name
+    before_pfp = before_pfp or member.avatar.url
+    arrow_padding = height // 6
+
+    arrow_image: Image.Image = open_as_image(
+        os.path.join("assets/arrows", ARROW_MAP["default"])
+    )
+    if arrow != "default":
+        if arrow in ARROW_MAP:
+            arrow_image = open_as_image(os.path.join("assets/arrows", ARROW_MAP[arrow]))
+        else:
+            arrow_image = open_as_image(arrow, is_url=True)
+            # TODO: Add error note if an error occurs
+
+    # TODO: For SVGs, resizing is supported without quality loss
+    arrow_height = height - (arrow_padding * 2)
+    arrow_width = int(arrow_image.size[0] * (arrow_height / arrow_image.size[1]))
+    arrow_image = arrow_image.resize((arrow_width, arrow_height))
+
+    member_item_width = int(height * MEMBER_ITEM_ASPECT_RATIO)
+    before_member_item = render_member(
+        member, pfp=before_pfp, name=before_name, height=height, width=member_item_width
+    )
+    after_member_item = render_member(member, height=height, width=member_item_width)
+
+    image = Image.new(
+        "RGBA",
+        (
+            (member_item_width * 2)
+            + (arrow_padding * 2)
+            + (border_padding * 2)
+            + arrow_width,
+            height + (border_padding * 2),
+        ),
+        (0, 0, 0, 0),
+    )
+    image.paste(
+        before_member_item, (border_padding, border_padding), before_member_item
+    )
+    image.paste(
+        arrow_image,
+        (
+            member_item_width + arrow_padding + border_padding,
+            arrow_padding + border_padding,
+        ),
+        arrow_image,
+    )
+    image.paste(
+        after_member_item,
+        (
+            member_item_width + (arrow_padding * 2) + border_padding + arrow_width,
+            border_padding,
+        ),
+        after_member_item,
+    )
+
+    return image
+
+
 async def send_alert(
     channel: discord.TextChannel,
     title: str,
     description: str,
     name: str,
     avatar: str,
-    files: None = None,
-    fallback_file: str | BytesIO = path.join(path.dirname(__file__), FALLBACK_IMAGE),
-) -> discord.Message: ...
-@overload
-async def send_alert(
-    channel: discord.TextChannel,
-    title: str,
-    description: str,
-    name: str,
-    avatar: str,
-    files: tuple[Optional[discord.File], Optional[discord.File]],
-    fallback_file: str | BytesIO = path.join(path.dirname(__file__), FALLBACK_IMAGE),
-) -> discord.Message: ...
-async def send_alert(
-    channel: discord.TextChannel,
-    title: str,
-    description: str,
-    name: str,
-    avatar: str,
-    files: Optional[tuple[Optional[discord.File], Optional[discord.File]]] = None,
-    fallback_file: str | BytesIO = path.join(path.dirname(__file__), FALLBACK_IMAGE),
+    diff_file: Optional[discord.File] = None,
 ) -> discord.Message | tuple[discord.Message, discord.Message]:
     embed = Embed(
         title=title,
@@ -512,19 +846,13 @@ async def send_alert(
     main_message = await channel.send(embed=embed)
 
     # No files to also send
-    if files is None:
+    if diff_file is None:
         return main_message
 
     # TODO: Send as thumbnail instead
-    if files[0] is None:
-        files = (discord.File(fallback_file, "before.png"), files[1])
-
-    if files[1] is None:
-        files = (files[0], discord.File(fallback_file, "before.png"))
-
     return (
         main_message,
-        await channel.send(files=cast(tuple[discord.File, discord.File], files)),
+        await channel.send(file=diff_file),
     )
 
 
@@ -555,8 +883,6 @@ async def check_alert_member(member: discord.Member):
     name_changed = last_name and last_name != member.display_name
     pfp_changed = last_pfp and last_pfp != member.display_avatar.url
 
-    send_images = False
-
     alert_title: str = ""
     alert_description: str = ""
 
@@ -570,8 +896,6 @@ async def check_alert_member(member: discord.Member):
             + quip.format("name _AND_ profile picture")
         )
 
-        send_images = True
-
     elif name_changed:
         quip: str = choice(FUNNY_MESSAGES["common"] + FUNNY_MESSAGES["name"])
 
@@ -579,7 +903,6 @@ async def check_alert_member(member: discord.Member):
         alert_description = (
             f"# From `{last_name}` ➡ `{member.display_name}`\n" + quip.format("name")
         )
-
     elif pfp_changed:
         quip: str = choice(FUNNY_MESSAGES["common"] + FUNNY_MESSAGES["avatar"])
 
@@ -589,31 +912,16 @@ async def check_alert_member(member: discord.Member):
             + quip.format("avatar")
         )
 
-        send_images = True
-
     if name_changed or pfp_changed:
         print(
             f"DETECTED CHANGE IN USER {member.name} in guild "
             + f"{member.guild.id} ({member.guild.name})"
         )
 
-        files: list[Optional[discord.File]] = [None, None]
-        if send_images:
-            try:
-                files[0] = discord.File(
-                    BytesIO(download_image(cast(str, last_pfp))),
-                    "before.png",
-                )
-            except Exception as e:
-                print(f"ERROR: COULD NOT DOWNLOAD FILE 0: '{last_pfp}'", e)
-
-            try:
-                files[1] = discord.File(
-                    BytesIO(await member.display_avatar.read()),
-                    "after.png",
-                )
-            except Exception as e:
-                print(f"ERROR: COULD NOT READ FILE 1: '{member.display_avatar.url}'", e)
+        DIFF_IMAGE_PATH = "tmp/diff_tmp.png"
+        diff_image = await render_member_diff(member, last_name, last_pfp, height=256)
+        # FIXME: Race conditions my occur!
+        diff_image.save(DIFF_IMAGE_PATH)
 
         await send_alert(
             channel,
@@ -621,7 +929,7 @@ async def check_alert_member(member: discord.Member):
             alert_description,
             (last_name or member.display_name) + f" ({member.name})",
             last_pfp or member.display_avatar.url,
-            files=(files[0], files[1]) if send_images else None,
+            diff_file=discord.File(DIFF_IMAGE_PATH, "diff.png"),
         )
 
     save_guild_data(
@@ -632,7 +940,13 @@ async def check_alert_member(member: discord.Member):
 
 
 if __name__ == "__main__":
-    if not path.exists(".env"):
+    if not os.path.exists("tmp/"):
+        os.mkdir("./tmp")
+
+    if not os.path.exists("save/"):
+        os.mkdir("./save")
+
+    if not os.path.exists(".env"):
         raise FileNotFoundError(".env file dose not exist!")
 
     secrets = dotenv_values(".env")
@@ -870,7 +1184,8 @@ if __name__ == "__main__":
             print(str(tb))
             # Fix for 2000 char max message
             # TODO: Send to log files and send log id instead
-            split_traceback = split_length(str(tb), 1993)  # -7 characters for codeblock
+            # -7 characters for codeblock
+            split_traceback = split_length(str(tb), 1993)
             for trace_part in split_traceback:
                 await interaction.followup.send(
                     "```" + trace_part + "\n```", ephemeral=True
